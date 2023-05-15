f9d84cc8af6011e4f619e6b55036fce5a0d00875
Ronan Pardo Soares <ronan.soares@gmail.com>
2023-05-15 15:59:06 -0300

    Fix change of tests

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 Idade = int(input("Digite uma Idade: "))
 if Idade < 0:
-    print("impossível!")
+    print("Impossível!")
 elif Idade < 18:
-    print("não precisa se alistar.")
+    print("Não precisa se alistar.")
 elif Idade >= 18 and Idade <= 65:
     print("Não esqueça de votar na próxima eleição.")
 elif Idade > 65:
     print("Vá descansar.")
 else:
-    print("eita!")
+    print("Eita!")
